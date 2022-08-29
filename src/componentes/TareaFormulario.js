728779b28a59f2ba94c0781a3d2540055b3bdd43
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-29 03:53:46 -0400

    preventDefault

--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -7,18 +7,22 @@ function TareaFormulario(props) {
 
   const manejarCambio = e => {
     setInput(e.target.value);
-    console.log(e.target.value);
   }
 
   const manejarEnvio = e => {
+    e.preventDefault(); // previene que no recarge la pj al enviar
+    
     const tareaNueva = {
       id: '12311',
-      text: 'Hola'
+      text: input,
+      completada: false
     }
   }
 
   return (
-    <form className='tarea-formulario'>
+    <form 
+    className='tarea-formulario'
+    onSubmit={manejarEnvio} >
       <input
         className="tarea-input"
         type='texto'
